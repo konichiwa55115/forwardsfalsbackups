513990be202796e1b345ad38ae9bfdc3b66b2543
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-19 16:34:05 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/konichiwa55115/Telegram_Forwarder /LazyDeveloper
+git clone https://github.com/konichiwa55115/forward26626226 /LazyDeveloper
 cd /LazyDeveloper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
